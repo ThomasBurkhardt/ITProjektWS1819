1d23af1df3496f07b87eca756bb7d294de1d7300
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2019-01-14 16:02:43 +0100

    Codesäuberung von .shared

--- ITProjektWS1819/src/de/hdm/itprojektws1819/shared/report/KommentarInfoReport.java
+++
@@ -1,9 +0,0 @@
-package de.hdm.itprojektws1819.shared.report;
-
-import java.io.Serializable;
-
-public class KommentarInfoReport extends CompositeReport implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
